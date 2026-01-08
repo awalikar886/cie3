c931cc6d76647f76dec79faf82bee2d2cc7b0544
awalikar886 <awalikar886@gmail.com>
2026-01-08 16:20:10 +0530

    Update account.py

--- account.py
+++ account.py
@@ -1,8 +1,8 @@
 def account_details(acc_no, hol_name, acc_type, bal):
     result = (
-        f"acc no: {acc_no}\n"
-        f"hol  Name: {hol_name}\n"
-        f"type: {acc_type}\n"
+        f"acc_no: {acc_no}\n"
+        f"hol_name: {hol_name}\n"
+        f"acc_type: {acc_type}\n"
         f"bal: {bal}\n"
     )
     return result
