499fb73ea31bbba455c06985694596d392bbce9c
awalikar886 <awalikar886@gmail.com>
2026-01-08 15:46:58 +0530

    Fix formatting of expected output in test

--- account_test.py
+++ account_test.py
@@ -4,7 +4,7 @@ def test_account_details():
     expected_output = (
         "acc_no: 141\n",
         "hol_Name: Akash\n",
-        "acc_type: "current"\n",
+        "acc_type: current\n",
         "bal: 4000\n"
     )
 
