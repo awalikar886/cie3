91bf34ed25b4d4e11f6cdc59eb7decd74305a6f7
awalikar886 <awalikar886@gmail.com>
2026-01-08 17:20:07 +0530

    Update test_account.py

--- test_account.py
+++ test_account.py
@@ -2,9 +2,9 @@ from account import account_details
 
 def test_account_details():
      expected_output = (
-         f"acc_no:141"\n
-        f"hol_name:akash"\n
-        f"acc_type:current"\n
+         f"acc_no:141"
+        f"hol_name:akash"
+        f"acc_type:current"
         f"bal:4000"
     )
      assert account_details(141,"akash","current",4000)==expected_output
