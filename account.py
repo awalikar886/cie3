0da2b0e8f70937dbece4b6418ce91da4589f07d3
awalikar886 <awalikar886@gmail.com>
2026-01-08 15:10:09 +0530

    account details

---
+++ account.py
@@ -0,0 +1,14 @@
+def account_details(acc_no,hol_name,acc_type,bal)
+    result=(
+        f"Account_number:{acc_no}\n"
+        f"Holder_name:{hol_name}\n"
+        f"Account_type:{acc_type}\n"
+        f"Balance:{bal}\n"
+    )
+
+if _name_ == "_main_":
+    Account_number:141;
+    Holder_name:"Akash";
+    Account_type:"current";
+    Balance:4000;
+print(acc_no,hol_name,acc_type,bal)
---
+++ account_test.py
@@ -0,0 +1,9 @@
+from account import account_details:
+def account_details():
+    expected_output=(
+        "Account_number:141"
+        "Holder_name:Akash"
+        "Account_type:current"
+        "Balance:4000"
+    )
+assert (141,"Akash","current,4000")==expected_output
