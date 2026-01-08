4b1f740e913cf071ba5b65f9063a304679537093
awalikar886 <awalikar886@gmail.com>
2026-01-08 15:33:58 +0530

    Update account.py

--- account.py
+++ account.py
@@ -1,14 +1,16 @@
-def account_details(acc,hol_name,acc_type,bal):
-    result=(
-        f"Account_number:{acc}\n"
-        f"Holder_name:{hol_name}\n"
-        f"Account_type:{acc_type}\n"
-        f"Balance:{bal}\n"
+ def account_details(acc_no, hol_name, acc_type, bal):
+    result = (
+        f"acc no: {acc_no}\n"
+        f"hol  Name: {hol_name}\n"
+        f"type: {acc_type}\n"
+        f"bal: {bal}\n"
     )
+    return result
 
-if __name__ == "__main__":
-    Account_number:141;
-    Holder_name:"Akash";
-    Account_type:"current";
-    Balance:4000;
-print(acc,hol_name,acc_type,bal)
+
+if _name_ == "_main_":
+    acc_no = "101"
+    hol_name = "akash"
+    acc_type = "current
+    bal = 55000
+    print(account_details(acc_no,hol_name,acc_type, bal))
