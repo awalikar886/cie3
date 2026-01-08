e21af098f9911dca249b6f98d95768774e420a9a
awalikar886 <awalikar886@gmail.com>
2026-01-08 15:45:04 +0530

    Update account_test.py

--- account_test.py
+++ account_test.py
@@ -2,9 +2,9 @@ from account import account_details
 
 def test_account_details():
     expected_output = (
-        "acc_no: 141\n"
-        "hol_Name: Akash\n"
-        "acc_type: "current"\n"
+        "acc_no: 141\n",
+        "hol_Name: Akash\n",
+        "acc_type: "current"\n",
         "bal: 4000\n"
     )
 
